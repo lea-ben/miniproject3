3567c7f859ae6a17ac8c746cad995582415afb57
lea-ben <leabenillouche@MacBook-Pro-de-Lea.local>
2021-01-28 00:14:24 +0200

    firstversion

---
+++ miniproject.js
@@ -0,0 +1,9 @@
+
+let body = document.querySelector("#div");
+let color_blocks = document.querySelectorAll("#container");
+let varone = document.querySelectorAll("#main*");
+let vartwo= document.getElementsByTagName("button");
+
+
+
+
